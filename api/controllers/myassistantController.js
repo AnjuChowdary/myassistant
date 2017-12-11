909a0db2d01f544699cbb71833bcad04cbd525bf
AnjuChowdary <anjuchowdary93@gmail.com>
2017-12-11 15:39:18 +0530

    checking length of array

--- api/controllers/myassistantController.js
+++ api/controllers/myassistantController.js
@@ -4,10 +4,10 @@ exports.createEmployee = function(req, res){
     // response = "This is a sample response from your webhook!";
     users.push(req.data);
     if(users.length>0){
-      var us = users[0];
+      String us = users[0];
       var usersLength = users.length;
       res.send(JSON.stringify({ "speech": usersLength + "Users has been added successfully",
-                                "displayText": "User"+ us + "has been added successfully"
+                                "displayText": "User "+ us + " has been added successfully"
     //"speech" is the spoken version of the response, "displayText" is the visual version
     }));
     }else{
