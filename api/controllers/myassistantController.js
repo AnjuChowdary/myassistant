adda073d64610cbd66eae0fdcdf8dc72d4c2aa4d
AnjuChowdary <anjuchowdary93@gmail.com>
2017-12-11 13:54:02 +0530

    response added

--- api/controllers/myassistantController.js
+++ api/controllers/myassistantController.js
@@ -2,6 +2,9 @@
 
 exports.createEmployee = function(req, res){
   Task.find({}, function(err, task) {
-    
+    response = "This is a sample response from your webhook!"
+    res.send(JSON.stringify({ "speech": response, "displayText": response
+  //"speech" is the spoken version of the response, "displayText" is the visual version
+  }));
   });
 };
