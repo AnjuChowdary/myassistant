4c3e88e6bba6e3774d0654085b33c1e69d380410
AnjuChowdary <anjuchowdary93@gmail.com>
2017-12-11 17:58:51 +0530

    sending button in response

--- api/controllers/myassistantController.js
+++ api/controllers/myassistantController.js
@@ -11,31 +11,27 @@ exports.createEmployee = function(req, res){
     // const app = new DialogflowApp({req, res});
     console.log(app);
     const hasScreen = app.hasSurfaceCapability(app.SurfaceCapabilities.SCREEN_OUTPUT);
-
+    console.log("hasScreenOutput"+hasScreen);
+    if(hasScreen){
+      function basicCard () {
+        app.ask(app.buildRichResponse()
+          // Create a basic card and add it to the rich response
+          .addSimpleResponse('New Employee Registration')
+          .addBasicCard(app.buildBasicCard('Employee Created Successfully.')
+            .setTitle('New Employee')
+            .addButton('Read more', 'https://example.google.com/mathandprimes')
+            .setImage('https://example.google.com/42.png', 'Image alternate text')
+            .setImageDisplay('CROPPED')
+          )
+        );
+    }
+  }else{
     if(users.length>0){
-      // String us = users[0];
-    //   var usersLength = users.length;
-    //   res.send(JSON.stringify({ "speech": "User " + users[0] + " has been added successfully",
-    //                             "displayText": "User "+ users[0] + " has been added successfully"
-    // //"speech" is the spoken version of the response, "displayText" is the visual version
-    // }));
-    res.send(JSON.stringify({ "speech": "Device hasScreen output " + hasScreen,
-                              "displayText": "User "+ hasScreen + " has been added successfully"
-  //"speech" is the spoken version of the response, "displayText" is the visual version
-  }));
-    // const app = new ActionsSdkApp({req, res});
-    function basicCard () {
-      app.ask(app.buildRichResponse()
-        // Create a basic card and add it to the rich response
-        .addSimpleResponse('New Employee Registration')
-        .addBasicCard(app.buildBasicCard('Employee Created Successfully.')
-          .setTitle('New Employee')
-          .addButton('Read more', 'https://example.google.com/mathandprimes')
-          .setImage('https://example.google.com/42.png', 'Image alternate text')
-          .setImageDisplay('CROPPED')
-        )
-      );
-  }
+
+        res.send(JSON.stringify({ "speech": "User " + users[0] + " has been added successfully",
+                                  "displayText": "User "+ users[0] + " has been added successfully"
+      //"speech" is the spoken version of the response, "displayText" is the visual version
+      }));
 
     }else{
       res.send(JSON.stringify({ "speech": "This is a sample response from your webhook!",
@@ -43,4 +39,6 @@ exports.createEmployee = function(req, res){
     //"speech" is the spoken version of the response, "displayText" is the visual version
     }));
     }
+  }
+
 };
