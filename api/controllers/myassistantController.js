bd4faab082875fd2167a4e0c4063574d33458cfe
AnjuChowdary <anjuchowdary93@gmail.com>
2017-12-11 14:56:32 +0530

    mongodb method deleted

--- api/controllers/myassistantController.js
+++ api/controllers/myassistantController.js
@@ -1,11 +1,9 @@
 'use strict';
 
 exports.createEmployee = function(req, res){
-  Task.find({}, function(err, task) {
     // response = "This is a sample response from your webhook!";
     res.send(JSON.stringify({ "speech": "This is a sample response from your webhook!",
                               "displayText": "This is a sample response from your webhook!"
   //"speech" is the spoken version of the response, "displayText" is the visual version
   }));
-  });
 };
