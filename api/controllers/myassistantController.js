6ce0b634d28b63aea8d53643ddda4b69ffe95d4a
AnjuChowdary <anjuchowdary93@gmail.com>
2017-12-11 17:50:39 +0530

    sending button in response

--- api/controllers/myassistantController.js
+++ api/controllers/myassistantController.js
@@ -3,7 +3,11 @@ var users = [];
 exports.createEmployee = function(req, res){
     // response = "This is a sample response from your webhook!";
     users.push(req.body.result.parameters['employeename']);
-    const app = require('actions-on-google').my-assistant-783d0;
+    // const app = require('actions-on-google').my-assistant-783d0;
+
+    const { DialogflowApp } = require('actions-on-google');
+    const app = new DialogflowApp({request: request, response: response});
+
     // const app = new DialogflowApp({req, res});
     console.log(app);
     const hasScreen = app.hasSurfaceCapability(app.SurfaceCapabilities.SCREEN_OUTPUT);
