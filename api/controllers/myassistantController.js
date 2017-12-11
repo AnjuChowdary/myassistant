5ce68d8c0a934c369d6c66b276faf8a0cac58efb
AnjuChowdary <anjuchowdary93@gmail.com>
2017-12-11 16:54:26 +0530

    sending button in response

--- api/controllers/myassistantController.js
+++ api/controllers/myassistantController.js
@@ -11,19 +11,19 @@ exports.createEmployee = function(req, res){
     //"speech" is the spoken version of the response, "displayText" is the visual version
     }));
     // const app = new ActionsSdkApp({req, res});
-  //   function basicCard () {
-  //     const app = require('actions-on-google').myassistant;
-  //     app.ask(app.buildRichResponse()
-  //       // Create a basic card and add it to the rich response
-  //       .addSimpleResponse('New Employee Registration')
-  //       .addBasicCard(app.buildBasicCard('Employee Created Successfully.')
-  //         .setTitle('New Employee')
-  //         .addButton('Read more', 'https://example.google.com/mathandprimes')
-  //         .setImage('https://example.google.com/42.png', 'Image alternate text')
-  //         .setImageDisplay('CROPPED')
-  //       )
-  //     );
-  // }
+    function basicCard () {
+      const app = require('actions-on-google').myassistant;
+      app.ask(app.buildRichResponse()
+        // Create a basic card and add it to the rich response
+        .addSimpleResponse('New Employee Registration')
+        .addBasicCard(app.buildBasicCard('Employee Created Successfully.')
+          .setTitle('New Employee')
+          .addButton('Read more', 'https://example.google.com/mathandprimes')
+          .setImage('https://example.google.com/42.png', 'Image alternate text')
+          .setImageDisplay('CROPPED')
+        )
+      );
+  }
 
     }else{
       res.send(JSON.stringify({ "speech": "This is a sample response from your webhook!",
