ddd000c7a5b4924e76e36f77aa1682764f8d0270
AnjuChowdary <anjuchowdary93@gmail.com>
2017-12-11 14:00:53 +0530

    response change

--- api/controllers/myassistantController.js
+++ api/controllers/myassistantController.js
@@ -2,7 +2,7 @@
 
 exports.createEmployee = function(req, res){
   Task.find({}, function(err, task) {
-    response = "This is a sample response from your webhook!"
+    response = "This is a sample response from your webhook!";
     res.send(JSON.stringify({ "speech": response, "displayText": response
   //"speech" is the spoken version of the response, "displayText" is the visual version
   }));
