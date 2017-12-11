c0bbca231cb783c7642b7f5b45f1a803210d0c0f
AnjuChowdary <anjuchowdary93@gmail.com>
2017-12-11 15:32:28 +0530

    fetching user name from array

--- api/controllers/myassistantController.js
+++ api/controllers/myassistantController.js
@@ -4,8 +4,9 @@ exports.createEmployee = function(req, res){
     // response = "This is a sample response from your webhook!";
     users.push(req.data);
     if(users.length>0){
-      res.send(JSON.stringify({ "speech": "User has been added successfully",
-                                "displayText": "User has been added successfully"
+      us = users[0];
+      res.send(JSON.stringify({ "speech": "User"+ us + "has been added successfully",
+                                "displayText": "User"+ us + "has been added successfully"
     //"speech" is the spoken version of the response, "displayText" is the visual version
     }));
     }else{
