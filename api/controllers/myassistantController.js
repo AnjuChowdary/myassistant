1274baa9e573c5be26da8d4b494e7bc42252c604
AnjuChowdary <anjuchowdary93@gmail.com>
2017-12-12 15:17:30 +0530

    sending button in response

--- api/controllers/myassistantController.js
+++ api/controllers/myassistantController.js
@@ -8,44 +8,57 @@ exports.createEmployee = function(req, res){
     const { DialogflowApp } = require('actions-on-google');
     const app = new DialogflowApp({request: req, response: res});
 
+    console.log(req.body.result['action']);
+    console.log(req.body.result['action'] == "createemployee");
+
     // console.log(app);
     console.log("users[] length: "+users.length);
     for(var i = 0;i<users.length;i++){
       console.log("User "+i+" "+users[i]);
     }
-    console.log("Users[0]: "+users[0]);
-    const hasScreen = app.hasSurfaceCapability(app.SurfaceCapabilities.SCREEN_OUTPUT);
-    console.log("hasScreenOutput"+hasScreen);
-    if(hasScreen){
 
-        console.log("basicCard Creation");
-        app.ask(app.buildRichResponse()
-          // Create a basic card and add it to the rich response
-          .addSimpleResponse('Employee '+users[users.length-1]+' registered successfully.')
-          .addBasicCard(app.buildBasicCard('VSoft’s development methodologies transform your requirements and objectives into innovative products with features that serve you well now and into the future. Our experienced project management teams strive for on-time delivery of your project, whether it is a single installation at a local institution; or a multi-phase, complex program for a data center or larger global financial institution.')
-            .setTitle('Vsofts Employee Registration')
-            .addButton('Read more about Vsoft Technologies', 'http://www.vsoftcorp.com/')
-            .setImage('https://restcountries.eu/data/ind.svg', 'Image alternate text')
-            .setImageDisplay('CROPPED')
-          )
-        );
-        console.log("Card Created");
+    if(req.body.result['action'] == "createemployee") {
+      const hasScreen = app.hasSurfaceCapability(app.SurfaceCapabilities.SCREEN_OUTPUT);
+      console.log("hasScreenOutput"+hasScreen);
+      if(hasScreen){
 
-    console.log("Basic card created successfully");
-  }else{
-    if(users.length>0){
+          console.log("basicCard Creation");
+          app.ask(app.buildRichResponse()
+            // Create a basic card and add it to the rich response
+            .addSimpleResponse('Employee '+users[users.length-1]+' registered successfully.')
+            .addBasicCard(app.buildBasicCard('VSoft’s development methodologies transform your requirements and objectives into innovative products with features that serve you well now and into the future. Our experienced project management teams strive for on-time delivery of your project, whether it is a single installation at a local institution; or a multi-phase, complex program for a data center or larger global financial institution.')
+              .setTitle('Vsofts Employee Registration')
+              .addButton('Read more about Vsoft Technologies', 'http://www.vsoftcorp.com/')
+              .setImage('https://restcountries.eu/data/ind.svg', 'Image alternate text')
+              .setImageDisplay('CROPPED')
+            )
+          );
 
-        res.send(JSON.stringify({ "speech": "User " + users[0] + " has been added successfully",
-                                  "displayText": "User "+ users[0] + " has been added successfully"
+          let actionMap = new Map();
+          actionMap.set(app.StandardIntents.MAIN, createEmployee);
+          actionMap.set(app.StandardIntents.TEXT, employeeDetails);
+
+        app.handleRequest(actionMap);
+          console.log("Card Created");
+
+      console.log("Basic card created successfully");
+    }else{
+      if(users.length>0){
+
+          res.send(JSON.stringify({ "speech": "User " + users[0] + " has been added successfully",
+                                    "displayText": "User "+ users[0] + " has been added successfully"
+        //"speech" is the spoken version of the response, "displayText" is the visual version
+        }));
+
+      }else{
+        res.send(JSON.stringify({ "speech": "This is a sample response from your webhook!",
+                                  "displayText": "This is a sample response from your webhook!"
       //"speech" is the spoken version of the response, "displayText" is the visual version
       }));
-
-    }else{
-      res.send(JSON.stringify({ "speech": "This is a sample response from your webhook!",
-                                "displayText": "This is a sample response from your webhook!"
-    //"speech" is the spoken version of the response, "displayText" is the visual version
-    }));
+      }
     }
-  }
+    }else{
+      console.log("No matching action found");
+    }
 
 };
